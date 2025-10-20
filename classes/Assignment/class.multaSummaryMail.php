c933bf3d45c455f40275a3690dfce42c76fd61df
Evan Jackson <evantjackson@gmail.com>
2025-10-20 08:58:15 -0400

    remove usless check

--- classes/Assignment/class.multaSummaryMail.php
+++ classes/Assignment/class.multaSummaryMail.php
@@ -164,11 +164,8 @@ class multaSummaryMail {
 		$this->renderEmailText();
 
 		$ilMail = new ilMail($ilUser->getId());
-		if (self::version()->is6()) {
-			$ilMail->sendMail($ilObjUser->getLogin(), '', '', $this->subject, $this->getMailText(), [], false);
-		} else {
-			$ilMail->sendMail($ilObjUser->getLogin(), '', '', $this->subject, $this->getMailText(), [], false);
-		}
+		
+		$ilMail->sendMail($ilObjUser->getLogin(), '', '', $this->subject, $this->getMailText(), [], false);
 	}
 
 
